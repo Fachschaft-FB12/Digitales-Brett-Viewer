37e21dce22ed3b945c91a1343e08ce58c688c68b
Robin Schumacher <info@robin-schumacher.com>
2022-01-14 23:46:28 +0100

    adjust python

--- python/rssfeed.py
+++ python/rssfeed.py
@@ -23,6 +23,6 @@ for post in xml.entries:
 if(len(embeds) > 0):
     response = requests.post("https://discord.com/api/webhooks/",
             json={
-                "content" : "**Auf dem Digitalen-Brett wurden " + len(embeds) + " neue Artikel veröffentlicht**:",
+                "content" : "**Auf dem Digitalen-Brett wurden " + str(len(embeds)) + " neue Artikel veröffentlicht**:",
                 "embeds": embeds
             })
