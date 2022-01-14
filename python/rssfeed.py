1f93c4b874eba2ce40071cf7eff82b9fdba9e3bf
Robin Schumacher <info@robin-schumacher.com>
2022-01-14 23:32:53 +0100

    add pyhton script for discord webhook

---
+++ python/rssfeed.py
@@ -0,0 +1,28 @@
+import feedparser
+from datetime import datetime,timedelta
+from time import mktime
+import re
+import requests
+
+xml = feedparser.parse("https://brett.fs-matheinfo.de/feed.xml")
+
+embeds = []
+
+for post in xml.entries:
+    dt = datetime.fromtimestamp(mktime(post.published_parsed))
+    last_hour_date_time = datetime.now() - timedelta(hours = 24)
+    if(last_hour_date_time < dt):
+        cleantext = re.sub('<[^<]+?>', '', post.description).strip()[:200] + "..."
+        embeds.append({
+            "title": post.title,
+            "description": cleantext,
+            "url": post.link,
+            "color": 15258703
+        })
+
+if(len(embeds) > 0):
+    response = requests.post("https://discord.com/api/webhooks/",
+            json={
+                "content" : "**Auf dem Digitalen-Brett wurden " + len(embeds) + " neue Artikel hochgeladen**:",
+                "embeds": embeds
+            })
